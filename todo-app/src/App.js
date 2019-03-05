dc571b3222c720f7514c485abe46360bb578eaa7
Maksim V <maks112v@gmail.com>
2019-03-05 16:52:53 -0500

    added nav bar

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import TodoList from './Components/List'
+import Navbar from './Components/Navbar'
 
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Navbar />
         <TodoList />
+        
       </div>
     );
   }
--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -7,6 +7,10 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers/index';
 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
 const store = createStore(reducer);
 
 ReactDOM.render(<Provider store={store} ><App /></Provider>, document.getElementById('root'));
